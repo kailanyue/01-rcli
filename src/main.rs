bf98c09e929ab9b7530485cdc18946517143d504
kailanyue <yuekailan@163.com>
2024-04-21 14:43:23 +0800

    remove Windows platform build conf

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rcli::{process_csv, Opts, SubCommand};
 
 /// `cargo run -- csv  -i ./assets/juventus.csv` 测试命令
 /// `cargo run -- csv  -i ./assets/juventus.csv -o ./assets/juventus.json` 测试命令
+
 fn main() -> Result<()> {
     let opts = Opts::parse();
     match opts.command {
@@ -11,3 +12,12 @@ fn main() -> Result<()> {
     }
     Ok(())
 }
+
+/*
+git tag -a v1-7-csv
+git push -u origin v1-7-csv
+
+
+git tag -d v1-7-csv
+git push origin --delete v1-7-csv
+*/
